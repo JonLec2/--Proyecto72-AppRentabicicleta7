5cbe70eb767596630c623ad30dec0097a7069bad
JonLec2 <92182897+JonLec2@users.noreply.github.com>
2022-08-09 19:11:39 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDnEP6Et0cXqiTXRhNswCYrhkpBUjgUBSM",
+  authDomain: "reantadebicicleapp.firebaseapp.com",
+  databaseURL: "https://reantadebicicleapp-default-rtdb.firebaseio.com",
+  projectId: "reantadebicicleapp",
+  storageBucket: "reantadebicicleapp.appspot.com",
+  messagingSenderId: "662938844301",
+  appId: "1:662938844301:web:28640cf39ce594a762f6c4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
